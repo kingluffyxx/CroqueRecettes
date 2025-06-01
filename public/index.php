d87fd67748e9890623669ef1f1b2bfadf0513d94
Xavier <xavier.adda@gmail.com>
2025-06-01 02:33:52 +0200

    test correction vercel index

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// Charge .env uniquement si présent (évite l’erreur sur Vercel)
+if (file_exists(dirname(__DIR__).'/.env')) {
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+}
+
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
