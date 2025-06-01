6f29a4765ef41f9fe37415f1ba77be68769de93a
Xavier <xavier.adda@gmail.com>
2025-06-01 04:04:22 +0200

    test

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,24 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+     public function getCacheDir(): string
+    {
+        // En environnement serverless (Vercel), /tmp est généralement writable
+        if ($this->environment !== 'dev') {
+            return sys_get_temp_dir().'/symfony/cache/'.$this->environment;
+        }
+
+        // Sinon, en local, on garde le dossier var/cache
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir(): string
+    {
+        if ($this->environment !== 'dev') {
+            return sys_get_temp_dir().'/symfony/log/'.$this->environment;
+        }
+
+        return parent::getLogDir();
+    }
 }
