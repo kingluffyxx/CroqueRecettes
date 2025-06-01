db8bc4c141e927599dc202f5c64a68da3740725d
Xavier <xavier.adda@gmail.com>
2025-06-01 20:44:25 +0200

    update csrf, package.json

--- src/Form/RegistrationForm.php
+++ src/Form/RegistrationForm.php
@@ -4,12 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -48,6 +46,8 @@ class RegistrationForm extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'csrf_token_id' => 'registration_form',
+            'csrf_protection' => true,
         ]);
     }
 }
