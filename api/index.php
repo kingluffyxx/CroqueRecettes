e26297b07abc0f93d4ee0ea986b27ad9520cc269
Xavier <xavier.adda@gmail.com>
2025-06-01 02:29:47 +0200

    test correction vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+require_once __DIR__ . '/../vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return require __DIR__ . '/../public/index.php';
+};
