5579191296a41be82961c9e99f738b0cbd5da91c
Xavier <xavier.adda@gmail.com>
2025-06-01 02:52:50 +0200

    rollback api/index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,3 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return require __DIR__ . '/../public/index.php';
-};
+require_once __DIR__ . '/../public/index.php';
